60a5e38be6772f67181c0bd9ca1a8db009717077
guilhermermonteiro <160605356+guilhermermonteiro@users.noreply.github.com>
2024-05-08 14:06:58 +0000

    Atividade de pyhton2

--- Média aritmética das notas informadas.py
+++ Média aritmética das notas informadas.py
@@ -1,2 +1,27 @@
 import os
+
 os.system("cls || clear")
+
+contadorNotas = 0
+soma = 0
+media=0
+
+while True:
+    nota=float(input(f"Digite uma nota (entre 8 e 10): "))
+    resposta=float(input(f"Deseja inserir mais uma nota? "))
+    
+    resposta=resposta.upper()
+
+
+    if resposta !="N":
+     break
+    
+    soma+=nota
+    contadorNotas+=1
+
+  
+  
+
+media=soma/contadorNotas
+
+print(f"Média: {media}")
